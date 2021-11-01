0bbe26c6136d2c1f6c58d84a3094aa8e17a84085
Darren Lin <darren@cabin.idv.tw>
2021-11-01 09:19:56 +0800

    Initial commit

---
+++ src/BatchEcsNodeTest/BatchEcsNodeTestStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace BatchEcsNodeTest
+{
+    public class BatchEcsNodeTestStack : Stack
+    {
+        internal BatchEcsNodeTestStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
